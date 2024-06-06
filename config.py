835cd73c5d689c5d977c0af83fb2b0780877a321
Ramo <albearatef@gmail.com>
2024-06-06 10:22:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Venomnay/ZeMusic-master",
+    "https://github.com/RamoBots/Ramo9.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
