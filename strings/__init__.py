c0682ff802e5df35e2c8df4b992de2f70f64c6de
Ramo <albearatef@gmail.com>
2024-06-09 17:00:15 +0300

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -19,7 +19,7 @@ for filename in os.listdir(r"./strings/langs/"):
         languages_present["ar"] = languages["ar"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "en":
+        if language_name == "ar":
             continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
